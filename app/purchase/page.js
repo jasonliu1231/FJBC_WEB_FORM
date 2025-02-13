45e0286cad28c8525c62d822fe88a4dfaa08ee65
jasonliu1231 <liuzaiyao1231@gmail.com>
2025-02-13 10:27:43 +0800

    修改表單輸入

--- app/purchase/page.js
+++ app/purchase/page.js
@@ -435,7 +435,7 @@ export default function Home() {
                   onChange={(event) => {
                     sendData.current.deadline = event.target.value;
                   }}
-                  type="datetime-local"
+                  type="date"
                   className="pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
